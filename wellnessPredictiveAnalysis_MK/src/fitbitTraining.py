a12388aba542a36d6c75e776c4d879f4bddeed02
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-06-11 21:36:30 -0400

    Correct label assignments.

--- wellnessPredictiveAnalysis_MK/src/fitbitTraining.py
+++ wellnessPredictiveAnalysis_MK/src/fitbitTraining.py
@@ -5,51 +5,57 @@ import pandas as pd
 
 def label_steps(dataframe):
     """Label the data according to daily steps."""
+    dataframe.Step_labels = dataframe.Step_labels.astype(str)
     for i, j in dataframe.iterrows():
         if j["Steps"] < 7500:
-            j["Step_labels"] = "LPA"
+            dataframe.at[i, "Step_labels"] = "LPA"
         if 7500 < j["Steps"] < 1000:
-            j["Step_labels"] = "MPA"
+            dataframe.at[i, "Step_labels"] = "MPA"
         if 10000 < j["Steps"] < 12500:
-            j["Step_labels"] = "HPA"
+            dataframe.at[i, "Step_labels"] = "HPA"
         if j["Steps"] > 12500:
-            j["Step_labels"] = "VHPA"
-        print(j["Step_labels"])
+            dataframe.at[i, "Step_labels"] = "VHPA"
+        # print(j["Step_labels"])
 
 
 def label_minutes_sitting(dataframe):
     """Label the data according to total minutes sitting daily."""
+    dataframe.Sitting_labels = dataframe.Sitting_labels.astype(str)
     for i, j in dataframe.iterrows():
         hours_sitting = j["Minutes_sitting"] / 60
         if hours_sitting < 4:
-            j["Sitting_labels"] = "LRMS"
+            dataframe.at[i, "Sitting_labels"] = "LRMS"
         if 4 < hours_sitting < 8:
-            j["Sitting_labels"] = "MRMS"
+            dataframe.at[i, "Sitting_labels"] = "MRMS"
         if 8 < hours_sitting < 11:
-            j["Sitting_labels"] = "HRMS"
+            dataframe.at[i, "Sitting_labels"] = "HRMS"
         if hours_sitting > 11:
-            j["Sitting_labels"] = "VHRMS"
+            dataframe.at[i, "Sitting_labels"] = "VHRMS"
         # print(j["Sitting_labels"])
 
 
 def label_physical_activity(dataframe):
     """Label data according to daily physical activity."""
+    dataframe.Activity_labels = dataframe.Activity_labels.astype(str)
+    # high_risk = "HRCD"
     for i, j in dataframe.iterrows():
         physical_activity = (
             j["Minutes_moderate_activity"] + j["Minutes_intense_activity"]
         )
         if physical_activity <= 15:
-            j["Activity_labels"] = "HRCD"
+            dataframe.at[i, "Activity_labels"] = "HRCD"
         if 15 < physical_activity <= 30:
-            j["Activity_labels"] = "MRCD"
+            dataframe.at[i, "Activity_labels"] = "MRCD"
         if physical_activity > 30:
-            j["Activity_labels"] = "LRCD"
+            dataframe.at[i, "Activity_labels"] = "LRCD"
         # print(j["Activity_labels"])
 
 
 # def combine_labels(dataframe):
+#     # print(dataframe.Activity_labels)
+#     # dataframe["Labels"] = dataframe["Steps_labels"].str.cat(dataframe["Sitting_labels"], sep=", ")
 #     for i, j in dataframe.iterrows():
-#         print(j["Labels"])
+#         print(j["Activity_labels"])
 
 
 if __name__ == "__main__":
@@ -57,4 +63,5 @@ if __name__ == "__main__":
     label_steps(fitbit_data)
     label_minutes_sitting(fitbit_data)
     label_physical_activity(fitbit_data)
+    print(fitbit_data)
     # combine_labels(fitbit_data)
