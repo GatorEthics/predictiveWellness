2b0b43fe0e5d2e29966c17c3b40000a1e760b1ec
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-06-13 13:52:00 -0400

    Combine health risks.

--- wellnessPredictiveAnalysis_MK/src/fitbitTraining2.py
+++ wellnessPredictiveAnalysis_MK/src/fitbitTraining2.py
@@ -9,7 +9,7 @@ def label_cardiovascular_disease(df):
             j["Minutes_moderate_activity"] + j["Minutes_intense_activity"]
         )
         hours_sitting = j["Minutes_sitting"] / 60
-        if physical_activity < 15 and hours_sitting > 8:
+        if physical_activity < 15 or hours_sitting > 8:
             df.at[i, "CD"] = "Cardiovascular disease"
 
 
@@ -17,7 +17,7 @@ def label_metabolic_syndrome(df):
     df.MS = df.MS.astype(str)
     for i, j in df.iterrows():
         hours_sitting = j["Minutes_sitting"] / 60
-        if j["Steps"] < 5000 and hours_sitting > 8:
+        if j["Steps"] < 5000 or hours_sitting > 8:
             df.at[i, "MS"] = "Metabolic syndrome"
 
 
@@ -27,30 +27,47 @@ def label_diabetes(df):
         physical_activity = (
             j["Minutes_moderate_activity"] + j["Minutes_intense_activity"]
         )
-        if physical_activity < 15 and j["Steps"] < 5000:
+        if physical_activity < 15 or j["Steps"] < 5000:
             df.at[i, "Diabetes"] = "Type II Diabetes"
 
 
 def label_good_health(df):
     df.Health = df.Health.astype(str)
     for i, j in df.iterrows():
-        physical_activity = (
-            j["Minutes_moderate_activity"] + j["Minutes_intense_activity"]
-        )
-        hours_sitting = j["Minutes_sitting"] / 60
-        if physical_activity >= 30 and hours_sitting <= 4 and j["Steps"] >= 10000:
+        if j["CD"] == "nan" and j["MS"] == "nan" and j["Diabetes"] == "nan":
             df.at[i, "Health"] = "Good health"
+        # physical_activity = (
+        #     j["Minutes_moderate_activity"] + j["Minutes_intense_activity"]
+        # )
+        # hours_sitting = j["Minutes_sitting"] / 60
+        # if physical_activity >= 30 and hours_sitting <= 4 and j["Steps"] >= 10000:
+        #     df.at[i, "Health"] = "Good health"
 
 
-def label_moderate_health(df):
+def label_health_risks(df):
     df.Health = df.Health.astype(str)
+    health = " "
     for i, j in df.iterrows():
-        physical_activity = (
-            j["Minutes_moderate_activity"] + j["Minutes_intense_activity"]
-        )
-        hours_sitting = j["Minutes_sitting"] / 60
-        if 15 < physical_activity < 30 and 4 < hours_sitting < 8 and 7500 < j["Steps"] < 10000:
-            df.at[i, "Health"] = "Moderate Health"
+        if j["CD"] != "nan":
+            health = health + "Cardiovascular Disease, "
+        if j["MS"] != "nan":
+            health = health + "Metabolic Syndrome, "
+        if j["Diabetes"] != "nan":
+            health = health + "Type II Diabetes "
+        df.at[i, "Health"] = health
+        health = ""
+        # print(health)
+        
+
+# def label_moderate_health(df):
+#     df.Health = df.Health.astype(str)
+#     for i, j in df.iterrows():
+#         physical_activity = (
+#             j["Minutes_moderate_activity"] + j["Minutes_intense_activity"]
+#         )
+#         hours_sitting = j["Minutes_sitting"] / 60
+#         if 15 <= physical_activity <= 30 and 4 <= hours_sitting <= 8 and 7500 <= j["Steps"] <= 10000:
+#             df.at[i, "Health"] = "Moderate Health"
 
 
 if __name__ == "__main__":
@@ -59,5 +76,6 @@ if __name__ == "__main__":
     label_metabolic_syndrome(fitbit_data)
     label_diabetes(fitbit_data)
     label_good_health(fitbit_data)
-    label_moderate_health(fitbit_data)
+    # label_moderate_health(fitbit_data)
+    label_health_risks(fitbit_data)
     fitbit_data.to_csv("updated_data.csv")
