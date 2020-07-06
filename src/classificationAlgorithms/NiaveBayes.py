12a62e182a1240a1703b5653ccde9a4f26907dca
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-07-06 15:56:22 -0400

    Correct data splitting in NiaveBayes.py.

--- src/classificationAlgorithms/NiaveBayes.py
+++ src/classificationAlgorithms/NiaveBayes.py
@@ -2,21 +2,23 @@
 
 import pandas as pd 
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GuassianNB
+from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 
 
 dataset = pd.read_csv("testingData.csv")
 
 # TODO: Split data in target and features
+x = dataset.drop("Health", axis=1)
+y = dataset["Health"]
 
 # Split features and target (x, y) into training and testing
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4)
 
 # Train the model with the training set
-guassian = GuassianNB()
-guassian = guassian.fit(x_train, y_train)
+gaussian = GaussianNB()
+gaussian = gaussian.fit(x_train, y_train)
 
 # Make predictions on the testing set
-prediction = guassian.predict(x_test)
+prediction = gaussian.predict(x_test)
 print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, prediction)*100)
