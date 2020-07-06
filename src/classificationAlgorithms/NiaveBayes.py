d5899b1c0d7f8d8081f8f0ccd981b0be5ab47b45
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-07-06 16:40:08 -0400

    Add functions to BiaveBayes.py.

--- src/classificationAlgorithms/NiaveBayes.py
+++ src/classificationAlgorithms/NiaveBayes.py
@@ -6,19 +6,55 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 
 
-dataset = pd.read_csv("testingData.csv")
+def import_data():
+    # Import csv file of individual data as pandas dataframe to use for training/testing data
+    dataset = pd.read_csv("testingData.csv")
+    # Print the dataset shape
+    print("Dataset Length: ", len(dataset))
+    print("Dataset Shape: ", dataset)
+    # Return data
+    return dataset
 
-# TODO: Split data in target and features
-x = dataset.drop("Health", axis=1)
-y = dataset["Health"]
 
-# Split features and target (x, y) into training and testing
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4)
+def split_data(dataset):
+    X = dataset.drop("Health", axis=1)
+    y = dataset["Health"]
 
-# Train the model with the training set
-gaussian = GaussianNB()
-gaussian = gaussian.fit(x_train, y_train)
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Make predictions on the testing set
-prediction = gaussian.predict(x_test)
-print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, prediction)*100)
+    return X, y, x_train, x_test, y_train, y_test
+
+
+def classify(x_train, y_train):
+    # Train the model with the training set
+    gaussian = GaussianNB()
+    gaussian = gaussian.fit(x_train, y_train)
+
+    return gaussian
+
+
+def predict(classifier, x_test):
+    GH = 0
+    MS = 0
+    # Make predictions on the testing set
+    prediction = classifier.predict(x_test)
+
+    for i in prediction:
+        if i == 0:
+            GH = GH + 1
+        if i == 1:
+            MS = MS + 1
+
+    if GH > MS:
+        print("Overall good health")
+    else:
+        print("Overall Metabolic Syndrome")
+
+    print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, prediction)*100)
+
+
+if __name__ == "__main__":
+    data = import_data()
+    X, Y, x_train, x_test, y_train, y_test = split_data(data)
+    classifier = classify(x_train, y_train)
+    predict(classifier, x_test)
