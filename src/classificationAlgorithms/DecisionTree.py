ec9bf52725024ad6e1cf0d61ee557829e837e6de
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-07-07 10:07:09 -0400

    Modify testing and training split.

--- src/classificationAlgorithms/DecisionTree.py
+++ src/classificationAlgorithms/DecisionTree.py
@@ -28,16 +28,20 @@ def split_dataset(individual_data):
     # Y contains target variable
     # X = individual_data[feature_columns].values
     # Y = individual_data[target_column].values
-    X = individual_data.drop("Health", axis=1)
-    Y = individual_data["Health"]
+    # X = individual_data.drop("Health", axis=1)
+    # Y = individual_data["Health"]
+    data_predictors = ["Steps", "Minutes_sitting", "Minutes_physical_activity", "HR", "BP"]
+    X = individual_data[data_predictors]
+    Y = individual_data.Health
+
     # Split the dataset for training and testing purposes
     # Ratio of training to testing is 70:30, this can be changed with test_size
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=100)
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, random_state=0)
 
     return X, Y, x_train, y_train, x_test, y_test
 
 
-def train_with_gini(x_train, x_test, y_train):
+def train_with_gini(x_train, y_train):
     """A function to perform training with the giniIndex."""
     # Create the classifier object
     classifier = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=3, min_samples_leaf=5)
@@ -46,7 +50,7 @@ def train_with_gini(x_train, x_test, y_train):
     return classifier
 
 
-def entropy_train(x_train, x_test, y_train):
+def entropy_train(x_train, y_train):
     """A function to perform training with entropy."""
     # Create classifier object
     entropy_classifier = DecisionTreeClassifier(criterion="entropy", random_state=100, max_depth=3, min_samples_leaf=5)
@@ -73,8 +77,8 @@ if __name__ == "__main__":
     # Build
     data = import_data()
     X, Y, x_train, x_test, y_train, y_test = split_dataset(data)
-    gini_classifier = train_with_gini(x_train, x_test, y_train)
-    entropy_classifier = entropy_train(x_train, x_test, y_train)
+    gini_classifier = train_with_gini(x_train, y_train)
+    entropy_classifier = entropy_train(x_train, y_train)
 
     # Prediction with gini
     print("Results Using Gini Index: ")
