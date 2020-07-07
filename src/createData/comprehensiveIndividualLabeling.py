0fe070c2139832c0b4851e6d7557a740234011ad
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2020-07-07 18:42:49 -0400

    Change labeling program.

--- src/createData/comprehensiveIndividualLabeling.py
+++ src/createData/comprehensiveIndividualLabeling.py
@@ -5,18 +5,20 @@ import numpy as np
 from pymed import PubMed
 
 
-# def label_cardiovascular_disease(df):
-#     """Label cardiovascular disease risk based on physical activity and hours sitting."""
-#     df.CD = df.CD.astype(str)
-#     for i, j in df.iterrows():
-#         hours_sitting = j["Minutes_sitting"] / 60
-#         if j["Minutes_physical_activity"] < 30 and hours_sitting > 8:
-#             df.at[i, "CD"] = "Cardiovascular disease"
+def label_cardiovascular_disease(df):
+    """Label cardiovascular disease risk based on physical activity and hours sitting."""
+    df.CD = df.CD.astype(str)
+    for i, j in df.iterrows():
+        hours_sitting = j["Minutes_sitting"] / 60
+        if j["Minutes_physical_activity"] < 30 and hours_sitting > 8:
+            df.at[i, "CD"] = "Cardiovascular disease"
 
-#             # and j["BP"] >= 130 and j["HR"] >= 80
+            # and j["BP"] >= 130 and j["HR"] >= 80
 
 def add_columns(df):
+    df["CD"] = ""
     df["MS"] = ""
+    df["Diabetes"] = ""
     df["Health"] = ""
 
 
@@ -30,32 +32,30 @@ def label_metabolic_syndrome(df):
             # and j["BP"] >= 130
 
 
-# def label_diabetes(df):
-#     """Label type II diabetes risk based on physical activity and daily steps."""
-#     df.Diabetes = df.Diabetes.astype(str)
-#     for i, j in df.iterrows():
-#         if j["Minutes_physical_activity"] < 30 and j["Steps"] < 7500:
-#             df.at[i, "Diabetes"] = "Type II Diabetes"
-#             # and j["BP"] >= 120
+def label_diabetes(df):
+    """Label type II diabetes risk based on physical activity and daily steps."""
+    df.Diabetes = df.Diabetes.astype(str)
+    for i, j in df.iterrows():
+        if j["Minutes_physical_activity"] < 30 and j["Steps"] < 7500:
+            df.at[i, "Diabetes"] = "Type II Diabetes"
+            # and j["BP"] >= 120
 
 
 def label_health_risks(df):
     """Determine health risks based on labels, or lack of labels."""
-    df.Health = df.Health.astype(str)
-    health = -1
+    # df.Health = df.Health.astype(str)
+    health = 0
     for i, j in df.iterrows():
-        # if j["CD"] != "nan":
-        #     health = health + "Cardiovascular Disease, "
-        if j["MS"] != "nan":
-            health = 1
-        # if j["Diabetes"] != "nan":
-        #     health = health + "Type II Diabetes "
-        # if j["CD"] == "nan" and j["MS"] == "nan" and j["Diabetes"] == "nan":
-        #     health = "Good health"
-        if j["MS"] == "":
+        if j["CD"] != "":
+            health = health + 2
+        if j["MS"] != "":
+            health = health + 3
+        if j["Diabetes"] != "":
+            health = health + 4
+        if j["CD"] == "" and j["MS"] == "" and j["Diabetes"] == "":
             health = 0
         df.at[i, "Health"] = health
-        health = -1
+        health = 0
 
 
 # def create_labels(df):
@@ -81,9 +81,9 @@ def label_health_risks(df):
 
 def main(individual_data):
     add_columns(individual_data)
-    # # label_cardiovascular_disease(individual_data)
+    label_cardiovascular_disease(individual_data)
     label_metabolic_syndrome(individual_data)
-    # label_diabetes(individual_data)
+    label_diabetes(individual_data)
     label_health_risks(individual_data)
     # create_labels(individual_data)
     individual_data.to_csv("customIndividual.csv")
